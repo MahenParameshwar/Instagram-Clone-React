f4ccaf649f647ece3763dd308d6f92f50adf3579
vikash93825 <yadav.kk74780@gmail.com>
2020-11-23 18:13:56 +0530

    User post

---
+++ my-app/src/Components/Context/DataContextProvider.jsx
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+const DataContext = React.createContext();
+
+class DataContextProvider extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isAuth: false,
+      isLoading: false,
+      error: false,
+      posts: [],
+      users: [],
+    };
+    this.getPosts = this.getPosts.bind(this);
+    this.getUsers = this.getUsers.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+    axios
+      .get(`http://localhost:3004/posts`)
+      .then((res) => {
+        this.setState({
+          posts: [...res.data],
+          isLoading: false,
+          error: false,
+        });
+      })
+      .catch((err) => {
+        this.setState({
+          error: true,
+          isLoading: false,
+        });
+      });
+
+    axios
+      .get(`http://localhost:3004/users`)
+      .then((res) => {
+        this.setState({
+          users: [...res.data],
+          isLoading: false,
+          error: false,
+        });
+      })
+      .catch((err) => {
+        this.setState({
+          error: true,
+          isLoading: false,
+        });
+      });
+  }
+
+  getPosts() {
+    const { posts } = this.state;
+    return posts;
+  }
+  getUsers() {
+    const { users } = this.state;
+    return users;
+  }
+
+  render() {
+    const { isAuth, isLoading, error } = this.state;
+    const { getPosts, getUsers } = this;
+    const value = {
+      getPosts,
+      getUsers,
+      isAuth,
+      isLoading,
+      error,
+    };
+    return (
+      <DataContext.Provider value={value}>
+        {this.props.children}
+      </DataContext.Provider>
+    );
+  }
+}
+
+export { DataContext, DataContextProvider };
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from "react-router-dom"
+import { DataContextProvider } from './Components/Context/DataContextProvider';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <DataContextProvider>
+     <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>
+  </DataContextProvider>,
   document.getElementById('root')
 );
 
