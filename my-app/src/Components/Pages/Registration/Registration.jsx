3aef46a0b867639a310bb12865bce59c65436a44
anusha <ramaduguanusha123@gmail.com>
2020-11-23 20:16:23 +0530

    registration feature

--- my-app/src/Components/Pages/Registration/Registration.jsx
+++ my-app/src/Components/Pages/Registration/Registration.jsx
@@ -1,88 +1,84 @@
 import React, { Component } from "react";
 import axios from "axios";
-import {v4 as uuid} from "uuid"
+import { v4 as uuid } from "uuid";
 import { DataContext } from "../../Context/DataContextProvider";
 
-class Registration extends Component{
-    constructor(props){
-        super(props)
+class Registration extends Component {
+    constructor(props) {
+        super(props);
         this.state = {
-            email:"",
-            username:"",
-            password:""
-        }
+            email: "",
+            username: "",
+            password: "",
+        };
     }
 
     handleChange = (e) => {
-        let {name,value} = e.target;
+        let { name, value } = e.target;
         this.setState({
-          [name] : value
-        })
-        
-  
-      }
-  
-      handleSubmit = (e) => {
+            [name]: value,
+        });
+    };
+
+    handleSubmit = (e) => {
         e.preventDefault();
-        let {email,username,password} = this.state;
+        let { email, username, password } = this.state;
         let { addUserData } = this.context;
         let user_id = uuid();
         let payload = {
-          user_id,
-          email,
-          username,
-          password,
-          following_users:[],
-          avatar_img : "",
-          follower_count:""
-        }
-  
-        addUserData(payload)
-        // const { history } = this.props
-        // history.push("/")
-        
-  
-      }
+            user_id,
+            email,
+            username,
+            password,
+            following_users: [],
+            avatar_img: "",
+            follower_count: "",
+        };
 
-    render(){
-        let {email,username,password} = this.state;
+        addUserData(payload);
         
-        return(
+    };
+
+    render() {
+        let { email, username, password } = this.state;
+
+        return (
             <>
-                <form onSubmit = {this.handleSubmit}>
-                 <input 
-                  type = "email"
-                  value = {email}
-                  placeholder = "email"
-                  onChange = {this.handleChange}
-                  name = "email" required/> <br/>
-
-                <input 
-                  type = "username"
-                  value = {username}
-                  placeholder = "username"
-                  onChange = {this.handleChange}
-                  name = "username" required/> <br/>
-
-                <input 
-                  type = "password"
-                  value = {password}
-                  placeholder = "password"
-                  onChange = {this.handleChange}
-                  name = "password" required/> <br/>
-                
-                
-                <input type="submit" value = "sign up"/>
-                
-                
-
-
-            </form>
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        type="email"
+                        value={email}
+                        placeholder="email"
+                        onChange={this.handleChange}
+                        name="email"
+                        required
+                    />{" "}
+                    <br />
+                    <input
+                        type="username"
+                        value={username}
+                        placeholder="username"
+                        onChange={this.handleChange}
+                        name="username"
+                        required
+                    />{" "}
+                    <br />
+                    <input
+                        type="password"
+                        value={password}
+                        placeholder="password"
+                        onChange={this.handleChange}
+                        name="password"
+                        required
+                    />{" "}
+                    <br />
+                    <input type="submit" value="sign up" />
+                </form>
             </>
-        )
+        );
     }
 }
 
-Registration.contextType = DataContext
+Registration.contextType = DataContext;
 
-export {Registration}
+export { Registration };
